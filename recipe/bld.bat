8cb1d51426d5040a03056269d13dca4cff3b404a
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2025-01-10 13:24:21 +0000

    drop m2w64-specific lines

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,14 +1,13 @@
 REM From https://github.com/conda-forge/netcdf-fortran-feedstock/blob/main/recipe/bld.bat
-set "HOST=x86_64-w64-mingw32"
-set "CC=%HOST%-gcc.exe"
-set "FC=%HOST%-gfortran.exe"
+REM set "HOST=x86_64-w64-mingw32"
+REM set "CC=%HOST%-gcc.exe"
+REM set "FC=%HOST%-gfortran.exe"
 
 (
 echo [build_ext]
-echo cmake_opts=-DPython3_EXECUTABLE="%PYTHON%" -DCMAKE_C_COMPILER=%CC% -G "Ninja"
-echo compiler=%FC%
+echo cmake_opts=-DPython3_EXECUTABLE="%PYTHON%" -G "Ninja"
 ) > "%SRC_DIR%\setup.cfg"
 
-set CFLAGS=-DMS_WIN64
+REM set CFLAGS=-DMS_WIN64
 set CMAKE_BUILD_PARALLEL_LEVEL=%CPU_COUNT%
 python -m pip install --no-deps -v "%SRC_DIR%"
